13d0022bac5bde68a93db4c0c8152579011f395b
Sourav Routh <67389838+SouravR7@users.noreply.github.com>
2020-12-11 03:37:38 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 var http = require("http");
 
 const httpServer = http.createServer(handleServer);
+ 
 
-
-function handleServer(req, res) {
+function handleServer(rq, rs) {
   
-}
+    if(rq.url ==="/welcome")
+    {   
+        rs.writeHead(200, { 'Content-Type': 'text/plain' });
+        rs.write("Welcome to Dominos!");
+        rs.end();
+    } 
+    else if(rq.url ==="/contact")
+    {
+        rs.writeHead(200, { 'Content-Type': 'application/json' });
+        rs.write(JSON.stringify({
+            phone: '18602100000',
+            email: 'guestcaredominos@jublfood.com'
+            }));
+        rs.end();
+    }
+    else{
+        rs.writeHead(404, {'Content-Type': 'text/html'});
+        rs.end("404 Not Found");
+    }
+    
+  
 
+
+
+}
+httpServer.listen(8081);
 module.exports = httpServer;
